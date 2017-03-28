2f74e7b4351885a1f6fda0feaa883bc5c4955419
Roel1l <raa.guerand@student.avans.nl>
2017-03-28 18:26:10 +0200

    roteren meme

--- linearAlgebra/linearAlgebra/MainWindow.xaml.cs
+++ linearAlgebra/linearAlgebra/MainWindow.xaml.cs
@@ -59,11 +59,19 @@ namespace linearAlgebra
                     cubeMatrix.draw(cube);
                     break;
                 case Key.D:
-                    cubeMatrix = rotateMatrix(cubeMatrix, 10, 300, 300);
+                    cubeMatrix = rotate3D(cubeMatrix, 10, 10, 8, 6);
                     cubeMatrix.draw(cube);
                     break;
                 case Key.A:
-                    cubeMatrix = rotateMatrix(cubeMatrix, -10, 300, 300);
+                    cubeMatrix = rotate3D(cubeMatrix, -10, 10, 8, 6);
+                    cubeMatrix.draw(cube);
+                    break;
+                case Key.W:
+                    cubeMatrix = rotate3D(cubeMatrix, 10, -10, -8, 6);
+                    cubeMatrix.draw(cube);
+                    break;
+                case Key.S:
+                    cubeMatrix = rotate3D(cubeMatrix, -10, -10, -8, 6);
                     cubeMatrix.draw(cube);
                     break;
                 case Key.Z:
@@ -76,7 +84,7 @@ namespace linearAlgebra
 
             //textBox.Clear();
 
-            //for(int column = 1; column < cubeMatrix.columns; column++)
+            //for (int column = 1; column < cubeMatrix.columns; column++)
             //{
             //    textBox.Text += "X: " + Math.Round((double)cubeMatrix.get(1, column), 2) + ", ";
             //    textBox.Text += "Y: " + Math.Round((double)cubeMatrix.get(2, column), 2) + ", ";
@@ -195,7 +203,7 @@ namespace linearAlgebra
             return returnMatrix;
         }
 
-        private Matrix rotateMatrix(Matrix matrixToRotate, double degrees, double rotationPointX, double rotationPointY)
+        private Matrix rotate2D(Matrix matrixToRotate, double degrees, double rotationPointX, double rotationPointY)
         {
             double theta = Math.Sin(degrees * (Math.PI / 180.0));
             double cos = Math.Cos(theta);
@@ -214,6 +222,115 @@ namespace linearAlgebra
             Matrix translatedBack = translate(rotatedMatrix, rotationPointX, rotationPointY, 0);
 
             return translatedBack;
+        }
+
+        private Matrix rotate3D(Matrix matrixToRotate, double degrees, double rotationPointX, double rotationPointY, double rotationPointZ)
+        {
+            double theta = Math.Sin(degrees * (Math.PI / 180.0));
+            double t1 = Math.Atan2(rotationPointZ, rotationPointX);
+            double t2 = Math.Atan2(rotationPointY, Math.Sqrt((Math.Pow(rotationPointX, 2)+ Math.Pow(rotationPointZ, 2))));
+            double cos = Math.Cos(theta);
+            double sin = Math.Sin(theta);
+
+            Matrix M12 = BringToXAxis(t1, t2);
+
+            Matrix M3 = new Matrix(3, 3);
+            M3.set(1, 1, 1);
+            M3.set(1, 2, 0);
+            M3.set(1, 3, 0);
+            
+            M3.set(2, 1, 0);
+            M3.set(2, 2, cos);
+            M3.set(2, 3, -sin);
+            
+            M3.set(3, 1, 0);
+            M3.set(3, 2, sin);
+            M3.set(3, 3, cos);
+
+            Matrix M123 = multiplyMatrices(M3, M12);
+
+            Matrix M12345 = getBackToStartPosition(M123, t1, t2);
+
+
+
+            return multiplyMatrices(M12345, matrixToRotate);
+        }
+
+        private Matrix getBackToStartPosition(Matrix M123, double t1, double t2)
+        {
+            Matrix M5 = new Matrix(3, 3);
+            double sinT1 = Math.Sin(t1);
+            double cosT1 = Math.Cos(t1);
+            double sinT2 = Math.Sin(t2);
+            double cosT2 = Math.Cos(t2);
+
+            M5.set(1, 1, cosT1);
+            M5.set(1, 2, 0);
+            M5.set(1, 3, -sinT1);
+
+            M5.set(2, 1, 0);
+            M5.set(2, 2, 1);
+            M5.set(2, 3, 0);
+
+            M5.set(3, 1, sinT1);
+            M5.set(3, 2, 0);
+            M5.set(3, 3, cosT1);
+
+            Matrix M4 = new Matrix(3, 3);
+
+            M4.set(1, 1, cosT2);
+            M4.set(1, 2, -sinT2);
+            M4.set(1, 3, 0);
+
+            M4.set(2, 1, sinT2);
+            M4.set(2, 2, cosT2);
+            M4.set(2, 3, 0);
+
+            M4.set(3, 1, 0);
+            M4.set(3, 2, 0);
+            M4.set(3, 3, 1);
+
+            Matrix M1234 = multiplyMatrices(M4, M123);
+            Matrix M12345 = multiplyMatrices(M5, M1234);
+            return M12345;
+        }
+
+        private Matrix BringToXAxis(double t1, double t2)
+        {
+            Matrix M2 = new Matrix(3, 3);
+            double sinT1 = Math.Sin(t1);
+            double cosT1 = Math.Cos(t1);
+            double sinT2 = Math.Sin(t2);
+            double cosT2 = Math.Cos(t2);
+
+            M2.set(1, 1, cosT2);
+            M2.set(1, 2, sinT2);
+            M2.set(1, 3, 0);
+
+            M2.set(2, 1, -sinT2);
+            M2.set(2, 2, cosT2);
+            M2.set(2, 3, 0);
+
+            M2.set(3, 1, 0);
+            M2.set(3, 2, 0);
+            M2.set(3, 3, 1);
+
+            Matrix M1 = new Matrix(3, 3);
+
+            M1.set(1, 1, cosT1);
+            M1.set(1, 2, 0);
+            M1.set(1, 3, sinT1);
+
+            M1.set(2, 1, 0);
+            M1.set(2, 2, 1);
+            M1.set(2, 3, 0);
+
+            M1.set(3, 1, -sinT1);
+            M1.set(3, 2, 0);
+            M1.set(3, 3, cosT1);
+
+            Matrix M12 = multiplyMatrices(M2, M1);
+            return M12;
         }
 
         private double rowTimesColumn(double?[] row, double?[] column)
