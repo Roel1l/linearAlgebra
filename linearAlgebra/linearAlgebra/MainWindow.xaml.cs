e01d39cc706261a48607c305a11763ea3e9a5538
Roel1l <raa.guerand@student.avans.nl>
2017-03-30 15:02:47 +0200

    rotate om punt

--- linearAlgebra/linearAlgebra/MainWindow.xaml.cs
+++ linearAlgebra/linearAlgebra/MainWindow.xaml.cs
@@ -25,11 +25,9 @@ namespace linearAlgebra
         {
             InitializeComponent();
             initShapeMatrix();
-
         }
 
-
-
+        
         private void OnKeyDown(object sender, KeyEventArgs e)
         {
             switch (e.Key)
@@ -59,19 +57,19 @@ namespace linearAlgebra
                     cubeMatrix.draw(cube);
                     break;
                 case Key.D:
-                    cubeMatrix = rotate3D(cubeMatrix, 10, 10, 8, 6);
+                    cubeMatrix = rotate3D(cubeMatrix, 10, 0, 0, 0);
                     cubeMatrix.draw(cube);
                     break;
                 case Key.A:
-                    cubeMatrix = rotate3D(cubeMatrix, -10, 10, 8, 6);
+                    cubeMatrix = rotate3D(cubeMatrix, -10, 0, 0, 0);
                     cubeMatrix.draw(cube);
                     break;
                 case Key.W:
-                    cubeMatrix = rotate3D(cubeMatrix, 10, -10, -8, 6);
+                    cubeMatrix = rotate3D(cubeMatrix, 10, -0.5, -0.5, 0.5);
                     cubeMatrix.draw(cube);
                     break;
                 case Key.S:
-                    cubeMatrix = rotate3D(cubeMatrix, -10, -10, -8, 6);
+                    cubeMatrix = rotate3D(cubeMatrix, -10, 0.5, 0.5, 0.5);
                     cubeMatrix.draw(cube);
                     break;
                 case Key.Z:
@@ -228,106 +226,208 @@ namespace linearAlgebra
         {
             double theta = Math.Sin(degrees * (Math.PI / 180.0));
             double t1 = Math.Atan2(rotationPointZ, rotationPointX);
-            double t2 = Math.Atan2(rotationPointY, Math.Sqrt((Math.Pow(rotationPointX, 2)+ Math.Pow(rotationPointZ, 2))));
+            double t2 = Math.Atan2(rotationPointY, Math.Sqrt((Math.Pow(rotationPointX, 2) + Math.Pow(rotationPointZ, 2))));
             double cos = Math.Cos(theta);
             double sin = Math.Sin(theta);
+            double cosT2 = Math.Cos(t2);
+            double sinT2 = Math.Sin(t2);
 
-            Matrix M12 = BringToXAxis(t1, t2);
+            Matrix M12 = getM12(t1, rotationPointX, rotationPointY, rotationPointZ);
 
-            Matrix M3 = new Matrix(3, 3);
-            M3.set(1, 1, 1);
-            M3.set(1, 2, 0);
+            Matrix M3 = new Matrix(4, 4);
+            M3.set(1, 1, cosT2);
+            M3.set(1, 2, sinT2);
             M3.set(1, 3, 0);
+            M3.set(1, 4, 0);
             
-            M3.set(2, 1, 0);
-            M3.set(2, 2, cos);
-            M3.set(2, 3, -sin);
+            M3.set(2, 1, -sinT2);
+            M3.set(2, 2, cosT2);
+            M3.set(2, 3, 0);
+            M3.set(2, 4, 0);
             
             M3.set(3, 1, 0);
-            M3.set(3, 2, sin);
-            M3.set(3, 3, cos);
+            M3.set(3, 2, 0);
+            M3.set(3, 3, 1);
+            M3.set(3, 4, 0);
+            
+            M3.set(4, 1, 0);
+            M3.set(4, 2, 0);
+            M3.set(4, 3, 0);
+            M3.set(4, 4, 1);
 
             Matrix M123 = multiplyMatrices(M3, M12);
 
-            Matrix M12345 = getBackToStartPosition(M123, t1, t2);
+            Matrix M12345 = getM12345(M123, theta, t2);
 
+            Matrix M1234567 = getM1234567(M12345, t1, rotationPointX, rotationPointY, rotationPointZ);
 
+            matrixToRotate.rows = 4;
+            for (int i = 1; i <= matrixToRotate.columns; i++)
+            {
+                matrixToRotate.set(4, i, 1);
+            }
 
-            return multiplyMatrices(M12345, matrixToRotate);
+            Matrix returnMatrix = multiplyMatrices(M1234567, matrixToRotate);
+            returnMatrix.rows = 3;
+
+            return returnMatrix;
         }
 
-        private Matrix getBackToStartPosition(Matrix M123, double t1, double t2)
+        private Matrix getM1234567(Matrix M12345, double t1, double x, double y, double z)
         {
-            Matrix M5 = new Matrix(3, 3);
+            Matrix M6 = new Matrix(4, 4);
             double sinT1 = Math.Sin(t1);
             double cosT1 = Math.Cos(t1);
+
+            M6.set(1, 1, cosT1);
+            M6.set(1, 2, 0);
+            M6.set(1, 3, -sinT1);
+            M6.set(1, 4, 0);
+
+            M6.set(2, 1, 0);
+            M6.set(2, 2, 1);
+            M6.set(2, 3, 0);
+            M6.set(2, 4, 0);
+
+            M6.set(3, 1, sinT1);
+            M6.set(3, 2, 0);
+            M6.set(3, 3, cosT1);
+            M6.set(3, 4, 0);
+
+            M6.set(4, 1, 0);
+            M6.set(4, 2, 0);
+            M6.set(4, 3, 0);
+            M6.set(4, 4, 1);
+
+            Matrix M7 = new Matrix(4, 4);
+
+            M7.set(1, 1, 1);
+            M7.set(1, 2, 0);
+            M7.set(1, 3, 0);
+            M7.set(1, 4, x);
+            
+            M7.set(2, 1, 0);
+            M7.set(2, 2, 1);
+            M7.set(2, 3, 0);
+            M7.set(2, 4, y);
+            
+            M7.set(3, 1, 0);
+            M7.set(3, 2, 0);
+            M7.set(3, 3, 1);
+            M7.set(3, 4, z);
+            
+            M7.set(4, 1, 0);
+            M7.set(4, 2, 0);
+            M7.set(4, 3, 0);
+            M7.set(4, 4, 1);
+
+            Matrix M123456 = multiplyMatrices(M6, M12345);
+
+            return multiplyMatrices(M7, M123456);
+        }
+
+        private Matrix getM12345(Matrix M123, double theta, double t2)
+        {
+            Matrix M5 = new Matrix(4, 4);
+            double cosTheta = Math.Cos(theta);
+            double sinTheta = Math.Sin(theta);
             double sinT2 = Math.Sin(t2);
             double cosT2 = Math.Cos(t2);
 
-            M5.set(1, 1, cosT1);
-            M5.set(1, 2, 0);
-            M5.set(1, 3, -sinT1);
-
-            M5.set(2, 1, 0);
-            M5.set(2, 2, 1);
+            M5.set(1, 1, cosT2);
+            M5.set(1, 2, -sinT2);
+            M5.set(1, 3, 0);
+            M5.set(1, 4, 0);
+            
+            M5.set(2, 1, sinT2);
+            M5.set(2, 2, cosT2);
             M5.set(2, 3, 0);
-
-            M5.set(3, 1, sinT1);
+            M5.set(2, 4, 0);
+            
+            M5.set(3, 1, 0);
             M5.set(3, 2, 0);
-            M5.set(3, 3, cosT1);
+            M5.set(3, 3, 1);
+            M5.set(3, 4, 0);
+            
+            M5.set(4, 1, 0);
+            M5.set(4, 2, 0);
+            M5.set(4, 3, 0);
+            M5.set(4, 4, 1);
 
-            Matrix M4 = new Matrix(3, 3);
+            Matrix M4 = new Matrix(4, 4);
 
-            M4.set(1, 1, cosT2);
-            M4.set(1, 2, -sinT2);
+            M4.set(1, 1, 1);
+            M4.set(1, 2, 0);
             M4.set(1, 3, 0);
-
-            M4.set(2, 1, sinT2);
-            M4.set(2, 2, cosT2);
-            M4.set(2, 3, 0);
-
+            M4.set(1, 4, 0);
+            
+            M4.set(2, 1, 0);
+            M4.set(2, 2, cosTheta);
+            M4.set(2, 3, -sinTheta);
+            M4.set(2, 4, 0);
+            
             M4.set(3, 1, 0);
-            M4.set(3, 2, 0);
-            M4.set(3, 3, 1);
+            M4.set(3, 2, sinTheta);
+            M4.set(3, 3, cosTheta);
+            M4.set(3, 4, 0);
+            
+            M4.set(4, 1, 0);
+            M4.set(4, 2, 0);
+            M4.set(4, 3, 0);
+            M4.set(4, 4, 1);
 
             Matrix M1234 = multiplyMatrices(M4, M123);
             Matrix M12345 = multiplyMatrices(M5, M1234);
             return M12345;
         }
 
-        private Matrix BringToXAxis(double t1, double t2)
+        private Matrix getM12(double t1, double x, double y, double z)
         {
-            Matrix M2 = new Matrix(3, 3);
+            Matrix M2 = new Matrix(4, 4);
             double sinT1 = Math.Sin(t1);
             double cosT1 = Math.Cos(t1);
-            double sinT2 = Math.Sin(t2);
-            double cosT2 = Math.Cos(t2);
 
-            M2.set(1, 1, cosT2);
-            M2.set(1, 2, sinT2);
-            M2.set(1, 3, 0);
-
-            M2.set(2, 1, -sinT2);
-            M2.set(2, 2, cosT2);
+            M2.set(1, 1, cosT1);
+            M2.set(1, 2, 0);
+            M2.set(1, 3, sinT1);
+            M2.set(1, 4, 0);
+            
+            M2.set(2, 1, 0);
+            M2.set(2, 2, 1);
             M2.set(2, 3, 0);
-
-            M2.set(3, 1, 0);
+            M2.set(2, 4, 0);
+            
+            M2.set(3, 1, -sinT1);
             M2.set(3, 2, 0);
-            M2.set(3, 3, 1);
+            M2.set(3, 3, cosT1);
+            M2.set(3, 4, 0);
+            
+            M2.set(4, 1, 0);
+            M2.set(4, 2, 0);
+            M2.set(4, 3, 0);
+            M2.set(4, 4, 1);
 
-            Matrix M1 = new Matrix(3, 3);
+            Matrix M1 = new Matrix(4, 4);
 
-            M1.set(1, 1, cosT1);
+            M1.set(1, 1, 1);
             M1.set(1, 2, 0);
-            M1.set(1, 3, sinT1);
+            M1.set(1, 3, 0);
+            M1.set(1, 4, -x);
 
             M1.set(2, 1, 0);
             M1.set(2, 2, 1);
             M1.set(2, 3, 0);
+            M1.set(2, 4, -y);
 
-            M1.set(3, 1, -sinT1);
+            M1.set(3, 1, 0);
             M1.set(3, 2, 0);
-            M1.set(3, 3, cosT1);
+            M1.set(3, 3, 1);
+            M1.set(3, 4, -z);
+
+            M1.set(4, 1, 0);
+            M1.set(4, 2, 0);
+            M1.set(4, 3, 0);
+            M1.set(4, 4, 1);
 
             Matrix M12 = multiplyMatrices(M2, M1);
             return M12;
